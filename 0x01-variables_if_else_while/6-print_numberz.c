90e0af1b858049d1a5fd9cebef67850c50ab68f8
lewis2022 <lewisite.web@gmail.com>
2022-03-14 00:27:31 -0700

    print from 0 to 10 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print using putchar all single digit number of bass 10 from 0.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
