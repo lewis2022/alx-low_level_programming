2f668a227eeeb84fa184c6fbdcf86e8c80c71061
lewis2022 <lewisite.web@gmail.com>
2022-03-14 00:12:26 -0700

    how to print numers on a base of 10 from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit number of bass 10 from 0.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
